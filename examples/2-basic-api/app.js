862072f932c7d783eb791f0f06ea3c71aec305ff
kriztiank <kriztiank@gmail.com>
2021-02-14 12:38:14 +0100

    init

---
+++ examples/2-basic-api/app.js
@@ -0,0 +1,30 @@
+const result = document.querySelector('.result');
+
+const fetchData = async () => {
+  try {
+    const { data } = await axios.get('/api/2-basic-api');
+    // console.log(data);
+    const products = data
+      .map((product) => {
+        // console.log(product);
+        const {
+          image: { url },
+          name,
+          price,
+        } = product;
+        return `<article class="product">
+      <img src="${url}" alt="${name}"/>
+      <div class="info">
+        <h5>${name}</h5>
+        <h5 class="price">$${price}</h5>
+      </div>
+      </article>`;
+      })
+      .join('');
+    result.innerHTML = products;
+  } catch (error) {
+    result.innerHTML = `<h4>There was an error. Please try again later</h4>`;
+  }
+};
+
+fetchData();
---
+++ functions/2-basic-api.js
@@ -0,0 +1,9 @@
+// const items = [{ name: 'susan' }, { name: 'anna' }];
+const items = require('../assets/data')
+
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(items),
+  };
+};
